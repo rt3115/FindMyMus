1f211f111feb35facc36b927997cafc3d9abd0c0
rt3115@rit.edu <Maggie4182>
2018-10-04 14:39:53 -0400

    Init

---
+++ app/src/main/java/com/example/happy/findmymus/FindLocalConActivity.java
@@ -0,0 +1,13 @@
+package com.example.happy.findmymus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindLocalConActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_local_con);
+    }
+}
