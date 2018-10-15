5dc95882e0ac5734b9bf6bb899e3cd44f91dc718
rt3115@rit.edu <Maggie4182>
2018-10-15 17:47:26 -0400

    YES

--- app/src/main/java/com/example/happy/findmymus/MainActivity.java
+++ app/src/main/java/com/example/happy/findmymus/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.happy.findmymus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mapActivityButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapActivityButton = (Button) findViewById(R.id.Profile);
+
+        mapActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openProfileActivity();
+                // openMapActivity();
+            }
+        });
+    }
+
+    public void openMapActivity()
+        {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openProfileActivity(){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
     }
 }
