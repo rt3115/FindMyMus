a79dbf43b97177ca8f95816ea378d044d85aad7f
Mark <mam3745@g.rit.edu>
2018-10-15 16:01:13 -0400

    I updated my Branch, I attempted to code the Profile Button in the Main Activity but so far nothing

--- app/src/main/java/com/example/happy/findmymus/DirectMessage.java
+++ app/src/main/java/com/example/happy/findmymus/DirectMessage.java
@@ -2,12 +2,34 @@ package com.example.happy.findmymus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class DirectMessage extends AppCompatActivity {
-
+    private Button button;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_direct_message);
+
+        button = (Button) findViewById(R.id.Profile);
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                openProfileActivity();
+            }
+        });
+    }
+
+    public void openProfileActivity()
+    {
+        Intent intent = new Intent ( packageContext: this, ProfileActivity.class);
+        startActivity(intent);
     }
 }
