559c8f0a55ece59f9b3d4c546736745542dbfb89
rt3115@rit.edu <Maggie4182>
2018-10-16 13:15:59 -0400

    More Stuff

--- app/src/main/java/com/example/happy/findmymus/FindLocalConActivity.java
+++ app/src/main/java/com/example/happy/findmymus/FindLocalConActivity.java
@@ -1,13 +1,26 @@
 package com.example.happy.findmymus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class FindLocalConActivity extends AppCompatActivity {
+
+    Button searchbarButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_local_con);
+
+        searchbarButton = (Button) findViewById(R.id.search_bar);
+
+
+    }
+
+    public void openMapsSearch(){
+        Intent intent = new Intent(this, SearchMapActivity.class);
+        startActivity(intent);
     }
 }
