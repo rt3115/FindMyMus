a5254c2652680569ecb4c374d15139caa5d59b37
rt3115@rit.edu <Maggie4182>
2018-10-16 07:50:48 -0400

    Added profile and settings button, if they are on a window they should now work

--- app/src/main/java/com/example/happy/findmymus/FindMusician.java
+++ app/src/main/java/com/example/happy/findmymus/FindMusician.java
@@ -1,13 +1,33 @@
 package com.example.happy.findmymus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FindMusician extends AppCompatActivity {
+
+    private Button profileActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_musician);
+
+        profileActivityButton = (Button) findViewById(R.id.Profile);
+
+        profileActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openProfileActivity();
+            }
+        });
+
+    }
+
+    public void openProfileActivity(){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/happy/findmymus/ProfileActivity.java
+++ app/src/main/java/com/example/happy/findmymus/ProfileActivity.java
@@ -1,13 +1,33 @@
 package com.example.happy.findmymus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private Button settingsActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        settingsActivityButton = (Button) findViewById(R.id.Settings);
+
+        settingsActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSettingsActivity();
+            }
+        });
+    }
+
+    public void openSettingsActivity(){
+        Intent intent = new Intent(this, SettingsWindowActivity.class);
+        startActivity(intent);
     }
 }
