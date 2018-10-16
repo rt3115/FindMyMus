9361bd3c58169998dad09f180f402571f4ef02bf
rt3115@rit.edu <Maggie4182>
2018-10-16 14:05:23 -0400

    Added Constraints

--- app/src/main/java/com/example/happy/findmymus/FindLocalConActivity.java
+++ app/src/main/java/com/example/happy/findmymus/FindLocalConActivity.java
@@ -3,6 +3,7 @@ package com.example.happy.findmymus;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class FindLocalConActivity extends AppCompatActivity {
@@ -16,7 +17,12 @@ public class FindLocalConActivity extends AppCompatActivity {
 
         searchbarButton = (Button) findViewById(R.id.search_bar);
 
-
+        searchbarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMapsSearch();
+            }
+        });
     }
 
     public void openMapsSearch(){
