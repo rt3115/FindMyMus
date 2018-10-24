c730fd0ef8afc7af6e4f42ce90d4c8694b3df1c7
rt3115@rit.edu <Maggie4182>
2018-10-24 10:19:24 -0400

    Basic Log in Code, won't go to profile unless logged in from main, later will bring user right to log in and sign up activity

--- app/src/main/java/com/example/happy/findmymus/Profile/ProfileData.java
+++ app/src/main/java/com/example/happy/findmymus/Profile/ProfileData.java
@@ -9,7 +9,7 @@ public class ProfileData {
     private String name;
     private String userName;
 
-    private boolean L = true;
+    private String password; //this is not how you do this but it works for now
 
     public ProfileData(String Type, String loc, String description, String shortDescription){
         profileType = Type;
@@ -41,6 +41,7 @@ public class ProfileData {
     }
 
     public ProfileData(String Type){
+        //Debug setup, contains only Type
         profileType = Type;
     }
 
---
+++ app/src/main/java/com/example/happy/findmymus/Profile/ProfileMain.java
@@ -0,0 +1,7 @@
+package com.example.happy.findmymus.Profile;
+
+public class ProfileMain {
+    //Don't touch
+
+    public static LocalProfile currentProfile = new LocalProfile();
+}
