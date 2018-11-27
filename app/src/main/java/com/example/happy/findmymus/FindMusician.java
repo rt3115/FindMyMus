0644f59fa19217a7b80332b2837c235cf7150d53
rt3115@rit.edu <Maggie4182>
2018-11-27 15:52:45 -0500

    Modified register and log in java and xml to fit our use case better

--- app/src/main/java/com/example/happy/findmymus/FindMusician.java
+++ app/src/main/java/com/example/happy/findmymus/FindMusician.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.util.Random;
 
@@ -17,6 +18,7 @@ public class FindMusician extends AppCompatActivity {
     private ImageButton likeMusButton;
     private ImageButton noMusButton;
     private ImageView profile;
+    private TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,8 @@ public class FindMusician extends AppCompatActivity {
         likeMusButton = (ImageButton) findViewById(R.id.Like);
         noMusButton = (ImageButton) findViewById(R.id.No);
         profile = (ImageView) findViewById(R.id.profile_pic);
+        profile.setImageResource(R.drawable.musician_numba_1);
+        name = (TextView) findViewById(R.id.NameOfPerson);
 
         profileActivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,26 +77,37 @@ public class FindMusician extends AppCompatActivity {
 
         switch (n) {
             case 1: profile.setImageResource(R.drawable.musician_numba_1);
+            name.setText("OhHaiMark");
                 break;
             case 2: profile.setImageResource(R.drawable.musician_numba_2);
+                name.setText("");
                 break;
             case 3: profile.setImageResource(R.drawable.musician_numba_3);
+                name.setText("");
                 break;
             case 4: profile.setImageResource(R.drawable.musician_numba_4);
+                name.setText("");
                 break;
             case 5: profile.setImageResource(R.drawable.musician_numba_5);
+                name.setText("");
                 break;
             case 6: profile.setImageResource(R.drawable.musician_numba_6);
+                name.setText("");
                 break;
             case 7: profile.setImageResource(R.drawable.musician_numba_7);
+                name.setText("BestProf");
                 break;
             case 8: profile.setImageResource(R.drawable.musician_numba_8);
+                name.setText("");
                 break;
             case 9: profile.setImageResource(R.drawable.musician_numba_9);
+                name.setText("please_Dont_Give_Me_A_Zero");
                 break;
             case 10: profile.setImageResource(R.drawable.musician_numba_10);
+                name.setText("");
                 break;
             case 11: profile.setImageResource(R.drawable.musician_numba_11);
+                name.setText("");
                 break;
         }
 
